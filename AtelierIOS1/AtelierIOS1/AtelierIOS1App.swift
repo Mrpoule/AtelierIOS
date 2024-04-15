be1cfccc4febb81946f8256bf7d9242949f120bc
ILoveReact <57147249+LeRaide@users.noreply.github.com>
2024-04-15 14:15:43 -0400

    first push

---
+++ AtelierIOS1/AtelierIOS1/AtelierIOS1App.swift
@@ -0,0 +1,17 @@
+//
+//  AtelierIOS1App.swift
+//  AtelierIOS1
+//
+//  Created by Tech Info on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct AtelierIOS1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
